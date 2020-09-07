e8499e0ce96e7ebf023278b3ac93d7a3335bdfe6
Sandro Arpi <sandro.teste72@gmail.com>
2020-09-07 13:49:52 -0300

    Criacao do programa

--- Exerc/Program.cs
+++ Exerc/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExercicioEnum.Entities;
+using ExercicioEnum.Entities.Enums;
+using System;
+using System.Globalization;
 
 namespace Exercicio
 {
@@ -6,7 +9,24 @@ namespace Exercicio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+
+            Console.WriteLine("Enter worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior)");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.Write("How many contracts for this worker? ");
+
+
+
         }
     }
 }
